37de78b2094ab15e2bfabd327ded3f453a830742
katakk <kata@katakuri.sakura.ne.jp>
2015-11-09 10:48:33 -0700

    a last of obsd md5

--- config.h
+++ config.h
@@ -1,4 +1,4 @@
-//#define USE_WINCRIPTAPI 0  /* @  OpenBSD MD5 */
-#define USE_WINCRIPTAPI 1  /* @  CAPI MD5 */
+#define USE_WINCRIPTAPI 0  /* @  OpenBSD MD5 */
+//#define USE_WINCRIPTAPI 1  /* @  CAPI MD5 */
 
 #define BENCH_MARK 0
